d1f924fdef867a729a62475d7824fac8424f3529
Rabindra Khadka <rabindra.khadka@91signage.com>
2026-02-25 17:35:06 +0545

    Releasing verioncode 19 and version name 1.1.7 on android and ios

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.bansalimmigration.clientportal"
         minSdk = 23
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.1.6"
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
     }
 
     signingConfigs {
